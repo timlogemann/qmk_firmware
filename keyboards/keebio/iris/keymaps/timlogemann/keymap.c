e2422ae882890d17a6a38aacda85eca9eba0951c
Tim Logemann <15154878+timlogemann@users.noreply.github.com>
2021-05-04 09:45:09 +0200

    - Remove layer names as variables to preserve space. - Turn Right ALT into Layer Toggle with [{ on tap - Turn Hyper Grave into Layer Toggle with Grave on tap.

--- keyboards/keebio/iris/keymaps/timlogemann/keymap.c
+++ keyboards/keebio/iris/keymaps/timlogemann/keymap.c
@@ -1,36 +1,31 @@
 #include QMK_KEYBOARD_H
 
-#define _DEFAULT 0
-#define _MOD 1
-#define _MOUSE 2
-#define _NUMPAD 3
-
-#define HP_GR HYPR_T(KC_GRAVE)
-#define MH_DT MEH_T(KC_DOT)
-#define SHFT_CPS LSFT_T(OSM(MOD_LSFT))
 #define _ KC_NO
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_DEFAULT] = LAYOUT(
+    // Default
+    [0] = LAYOUT(
         //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
             KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,                               KC_6,       KC_7,   KC_8,  KC_9,    KC_0,   KC_BSPC,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
             KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,                               KC_Y,    KC_U,      KC_I,   KC_O,   KC_P,    KC_EQUAL,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            SHFT_CPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,                               KC_H,    KC_J,      KC_K,   KC_L,   KC_SCLN, KC_QUOT,
+            KC_LSFT, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,                               KC_H,    KC_J,      KC_K,   KC_L,   KC_SCLN, KC_QUOT,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-            KC_LCTL, HP_GR,    KC_Z,    KC_X,   KC_C,   KC_V,       _,                _,    KC_B,    KC_N,      KC_M,   KC_COMM, MH_DT,  KC_SLSH,
+            KC_LCTL, LT(4, KC_GRAVE),    KC_Z,   KC_X,   KC_C,   KC_V,       _,                _,    KC_B,    KC_N,      KC_M,   KC_COMM, KC_DOT,  KC_SLSH,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┤        ┤        ├        ┼────┬───┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                            KC_LAPO, KC_LGUI,     LT(_NUMPAD, KC_ENT), KC_SPC,   TT(_MOD), KC_RAPC
+                                            KC_LAPO, KC_LGUI,     LT(3, KC_ENT), KC_SPC,   TT(1), LT(4,  KC_LBRC)
                                        //└────────┴────────┘    └────────┘        └────────┘    └───────┴────────┘
         ),
-
-    [_MOD] = LAYOUT(
+    //
+    [1] = LAYOUT(
         //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-            KC_GRAVE,   KC_F1,   KC_F2,    KC_F3,  KC_F4,     _,                                  _,       KC_MEDIA_PREV_TRACK,    KC_MEDIA_PLAY_PAUSE,    KC_MEDIA_NEXT_TRACK,      _,      KC_DEL,
+            KC_GRAVE,   KC_F1,   KC_F2,    KC_F3,  KC_F4,     _,                                  _,       KC_MEDIA_PREV_TRACK,    KC_MEDIA_PLAY_PAUSE,    KC_MEDIA_NEXT_TRACK,      KC_RBRC,      KC_DEL,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            KC_TRNS,  KC_F5,   KC_F6,  KC_F7,  KC_F8,   TT(_MOUSE),                          KC_VOLU,    KC_PGUP,  KC_UP, KC_PGDN,    _,     KC_MINUS,
+            KC_TRNS,  KC_F5,   KC_F6,  KC_F7,  KC_F8,   TT(2),                          KC_VOLU,    KC_PGUP,  KC_UP, KC_PGDN,    _,     KC_MINUS,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
             KC_TRNS,   KC_F9,   KC_F10, KC_F11, KC_F12,  _,                                  KC_VOLD,    KC_LEFT, KC_DOWN, KC_RGHT,    _,     KC_PAST,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -39,19 +34,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 LALT_T(KC_LBRC),    KC_TRNS,    KC_TRNS,            KC_TRNS,     KC_TRNS, RALT_T(KC_RBRC)
                                        //└────────┴────────┘    └────────┘        └────────┘    └───────┴────────┘
         ),
-    [_MOUSE] = LAYOUT(
+    [2] = LAYOUT(
         //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-        TO(_DEFAULT),   _,    KC_TRNS,  _,       _,      RGB_TOG,                             _,       _,       _,       _,       _,        _,
+            TO(0),     _,    KC_TRNS,    _,       _,     RGB_TOG,                               _,       _,       _,       _,       _,        _,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            KC_TRNS,    _,      _,       _,       _,       _,                                KC_MS_WH_UP,    KC_MS_BTN1,   KC_MS_UP,   KC_MS_BTN2,      _,      _,
+            KC_TRNS,   _,      MAGIC_TOGGLE_CTL_GUI, _, _, _,                              KC_MS_WH_UP, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, _, _,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            KC_TRNS,    _,      _,       _,       _,       _,                                KC_MS_WH_DOWN,    KC_MS_L,   KC_MS_D,   KC_MS_R,      _,      _,
+            KC_TRNS,   _,       _,       _,       _,       _,                                KC_MS_WH_DOWN, KC_MS_L, KC_MS_D, KC_MS_R, _,   _,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-              _,    KC_LEAD,   KC_TRNS,   _,         _,      _,        _,                _,       _,      KC_MS_WH_LEFT,    _,      KC_MS_WH_RIGHT,    _,       _,
+              _,    KC_LEAD, KC_TRNS,    _,       _,       _,       _,                _,       _,     KC_MS_WH_LEFT, _, KC_MS_WH_RIGHT,    _, _,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┤        ┤        ├        ┼────┬───┴───┬────┴───┬────┴────────┴────────┴────────┘
                                             _,      RGB_RMOD,     KC_TRNS,           KC_TRNS,    RGB_MOD,   _
                                        //└────────┴────────┘    └────────┘        └────────┘    └───────┴────────┘
         ),
-    [_NUMPAD] = LAYOUT(MO(5),KC_TRNS,KC_KP_7,KC_KP_8,KC_KP_9,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_KP_4,KC_KP_5,KC_KP_6,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_KP_1,KC_KP_2,KC_KP_3,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_KP_0,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS),
-    [4] = LAYOUT(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,MAGIC_TOGGLE_CTL_GUI,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT)
+    [3] = LAYOUT(KC_TRNS,KC_TRNS,KC_KP_7,KC_KP_8,KC_KP_9,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_KP_4,KC_KP_5,KC_KP_6,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_KP_1,KC_KP_2,KC_KP_3,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_KP_0,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS),
+    [4] = LAYOUT(
+        //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+            KC_ESC,     _,      _,       _,       _,       _,                                  _,       _,      _,      KC_LBRC, KC_RBRC,   _,
+        //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+            KC_TAB,     _,      _,      KC_UP,   _,        _,                                  _,       _,      _,        _,       _,       _,
+        //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+            KC_LSFT,    _,    KC_LEFT, KC_DOWN, KC_RGHT,   _,                                  _,       _,       _,       _,       _,       _,
+        //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+            KC_LCTL, KC_TRNS,   _,       _,       _,       _,       _,                _,       _,       _,      _,        _,       _,       _,
+        //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┤        ┤        ├        ┼────┬───┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                            KC_LAPO, KC_LGUI,     KC_SPC,           KC_ENT,         _,      _
+                                       //└────────┴────────┘    └────────┘        └────────┘    └───────┴────────┘
+        ),
 };
