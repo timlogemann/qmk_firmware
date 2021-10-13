34feebd14f818b2715d80266433d30d0b987d10a
Tim Logemann <tim@todaytomorrow.nl>
2021-10-13 12:12:38 +0200

    fix KC_RAPC  by moving LT(2) to KC_SCLN (;)

--- keyboards/keebio/iris/keymaps/timlogemann/keymap.c
+++ keyboards/keebio/iris/keymaps/timlogemann/keymap.c
@@ -60,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
             KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,                               KC_Y,    KC_U,      KC_I,   KC_O,   KC_P,    KC_EQUAL,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            MT(MOD_LSFT, KC_CAPSLOCK), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,                    KC_H,    KC_J,      KC_K,   KC_L,   KC_SCLN, KC_QUOT,
+            MT(MOD_LSFT, KC_CAPSLOCK), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,                    KC_H,    KC_J,      KC_K,   KC_L,   LT(2, KC_SCLN), KC_QUOT,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-            KC_LCTL, KC_GRAVE,  KC_Z,   KC_X,   KC_C,   KC_V,_,         _,    KC_B,    KC_N,      KC_M,   KC_COMM, MT(MOD_RCTL, KC_DOT),  KC_SLSH,
+            KC_LCTL, KC_GRAVE,  KC_Z,   KC_X,   KC_C,   KC_V,_,     _,               KC_B,    KC_N,    KC_M,   KC_COMM, MT(MOD_RCTL, KC_DOT),  KC_SLSH,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┤        ┤        ├        ┼────┬───┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                            KC_LAPO, KC_LGUI,     LT(3, KC_ENT),    KC_SPC,   LT(1, KC_ENT),      LT(2, KC_RAPC)
+                                            KC_LAPO, KC_LGUI,     LT(3, KC_ENT),    KC_SPC,   LT(1, KC_ENT),      KC_RAPC
                                        //└────────┴────────┘    └────────┘        └────────┘    └───────┴────────┘
         ),
     //
