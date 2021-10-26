cb3644feb19e8d3196417509c20aee8b0da99cdc
Tim Logemann <tim@todaytomorrow.nl>
2021-10-26 10:16:10 +0200

    refactor to use Key override for shift+bcksp=delete, rather than a combo

--- keyboards/keebio/iris/keymaps/timlogemann/keymap.c
+++ keyboards/keebio/iris/keymaps/timlogemann/keymap.c
@@ -2,55 +2,28 @@
 
 #define _ KC_NO
 
-/* Combos */
+/**
+ *  Combos
+ * */
 const uint16_t PROGMEM caplock_combo[] = {KC_LALT, KC_LSFT, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {COMBO(caplock_combo, KC_CAPS)};
 
+/**
+ *  Key overrides
+ * */
+// backspace+shift = delete
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPACE, KC_DELETE);
 
-// Initialize variable holding the binary
-// representation of active modifiers.
-uint8_t mod_state;
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // Store the current modifier state in the variable for later reference
-    mod_state = get_mods();
-    switch (keycode) {
-
-    case KC_BSPC:
-        {
-        // Initialize a boolean variable that keeps track
-        // of the delete key status: registered or not?
-        static bool delkey_registered;
-        if (record->event.pressed) {
-            // Detect the activation of either shift keys
-            if (mod_state & MOD_MASK_SHIFT) {
-                // First temporarily canceling both shifts so that
-                // shift isn't applied to the KC_DEL keycode
-                del_mods(MOD_MASK_SHIFT);
-                register_code(KC_DEL);
-                // Update the boolean variable to reflect the status of KC_DEL
-                delkey_registered = true;
-                // Reapplying modifier state so that the held shift key(s)
-                // still work even after having tapped the Backspace/Delete key.
-                set_mods(mod_state);
-                return false;
-            }
-        } else { // on release of KC_BSPC
-            // In case KC_DEL is still being sent even after the release of KC_BSPC
-            if (delkey_registered) {
-                unregister_code(KC_DEL);
-                delkey_registered = false;
-                return false;
-            }
-        }
-        // Let QMK process the KC_BSPC keycode as usual outside of shift
-        return true;
-    }
-
-    }
-    return true;
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
 };
 
 
+/**
+ *  Layer maps
+ * */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Default
